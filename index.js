d4af410b648cfc5bfa8df310b7aaa75d3e3e8c3a
Maryna-Korbet <m22v86k@gmail.com>
2023-02-18 07:07:28 +0200

    cli application

---
+++ index.js
@@ -0,0 +1,52 @@
+const { Command } = require("commander");
+
+const {
+    listContacts,
+    getContactById,
+    removeContact, 
+    addContact
+} = require ("./contacts");
+
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+const argv = program.opts();
+
+require('colors');
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      console.log('Array of all contacts:'.bgGreen);
+      const allContacts = await listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      console.log('Contact found:'.bgBlue);
+      const oneContact = await getContactById(id);
+      return console.log(oneContact);
+
+    case "add":
+      console.log('A contact has been added:'.bgWhite);
+      const newContact = await addContact({name, email, phone});
+      return console.log(newContact);
+
+    case "remove":
+      console.log('Contact has been deleted:'.bgYellow);
+      const deleteContact = await removeContact(id);
+      return console.log(deleteContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
+
